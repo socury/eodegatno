d51ae6a19400b26dedc282304d74e7f0a6c6b09c
socury <socury1104@gmail.com>
2025-11-24 09:39:33 +0900

    init

---
+++ src/main/java/com/dgsw/eodegatno/EodegatnoApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.eodegatno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EodegatnoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EodegatnoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dgsw/eodegatno/EodegatnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.eodegatno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EodegatnoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
