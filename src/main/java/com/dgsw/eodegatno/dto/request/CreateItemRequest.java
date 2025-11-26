ed7003c7870669b35fd459bbb6f705b535e946dd
socury <socury1104@gmail.com>
2025-11-26 20:05:50 +0900

    feat: add validation for contact information in CreateItemRequest and update SQL insert statements

--- src/main/java/com/dgsw/eodegatno/dto/request/CreateItemRequest.java
+++ src/main/java/com/dgsw/eodegatno/dto/request/CreateItemRequest.java
@@ -2,6 +2,8 @@ package com.dgsw.eodegatno.dto.request;
 
 import com.dgsw.eodegatno.domain.ItemEntity;
 import com.dgsw.eodegatno.domain.ItemStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,6 +24,8 @@ public class CreateItemRequest {
 
     private ItemStatus status;
 
+    @NotBlank(message = "연락처를 입력해주세요.")
+    @Pattern(regexp = "^010\\d{8}$", message = "연락처는 010으로 시작하는 11자리 숫자만 입력 가능합니다.")
     private String contactInfo;
 
     private String description;
